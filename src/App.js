43af94b391c7a6e4dd6a1354c2a387579e870281
Lu Ghidara <lughidara@gmail.com>
2020-08-19 09:12:33 -0300

    se agrego un componente condicional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
 
 function App() {
+  //crear state
+  const [fraseSimpsons, setFraseSimpsons] = useState({});
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  //la siguiente funcion tiene que ser asincrona porque no sabemos cto se puede tardar el servidor o la api, entonces usamos async y await
+  const consultarAPI = async () => {
+    setLoader(true);
+    const api = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes");
+    const fraseObtenida = await api.json();
+    //api.json me devuelve la parte de los datos json de api
+    //console.log(api);
+    console.log(fraseObtenida[0]);
+
+    setTimeout(() => {
+      setFraseSimpsons(fraseObtenida[0]);
+      setLoader(false);
+    }, 2000);
+  };
+
+  //creamos variable que guarda un componente, usamos operadores ternarios a la derecha
+  const componenteCondicional = loader ? (
+    <Spinner></Spinner>
+  ) : (
+    <Frase fraseSimpsons={fraseSimpsons}></Frase>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container text-center my-5">
+      <article className="d-flex flex-column my-5 align-items-center">
+        <img
+          src={process.env.PUBLIC_URL + "thesimpsons.png"}
+          alt="logo de los simpsons"
+          className="w-75"
+        ></img>
+        <button
+          className="btn btn-dark my-5 w-50 shadow"
+          onClick={() => consultarAPI()}
         >
-          Learn React
-        </a>
-      </header>
+          Obtener frase
+        </button>
+      </article>
+      {componenteCondicional}
     </div>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Frase = (props) => {
+    return (
+        <div>
+            <div className="card mb-3">
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img src={props.fraseSimpsons.image} className="card-img" alt={props.fraseSimpsons.character}></img>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{props.fraseSimpsons.character}</h5>
+                            <p className="card-text">{props.fraseSimpsons.quote}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Frase;   
